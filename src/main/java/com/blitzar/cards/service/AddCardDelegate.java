99671f98f700257fb57446d57e6e6a7998c58699
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-03-25 23:51:47 +0100

    Creating constants for immutable add card values

--- src/main/java/com/blitzar/cards/service/AddCardDelegate.java
+++ src/main/java/com/blitzar/cards/service/AddCardDelegate.java
@@ -1,6 +1,13 @@
 package com.blitzar.cards.service;
 
+import com.blitzar.cards.domain.CardStatus;
+
 public interface AddCardDelegate {
+
+    CardStatus DEFAULT_CARD_STATUS = CardStatus.BLOCKED;
+    int DEFAULT_DAILY_WITHDRAWAL_LIMIT = 1000;
+    int DEFAULT_DAILY_PAYMENT_LIMIT = 2500;
+    int DEFAULT_YEAR_PERIOD_EXPIRATION_DATE = 5;
+
     String getCardholderName();
-    Integer getDailyWithdrawalLimit();
 }
