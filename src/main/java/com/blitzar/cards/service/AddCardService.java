b534a735922469a0fd6bd79805d9faf3fe7d1834
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-03-25 23:56:42 +0100

    Add Card Rest API tests

--- src/main/java/com/blitzar/cards/service/AddCardService.java
+++ src/main/java/com/blitzar/cards/service/AddCardService.java
@@ -4,7 +4,6 @@ import am.ik.yavi.builder.ValidatorBuilder;
 import am.ik.yavi.core.ConstraintViolationsException;
 import com.blitzar.cards.domain.Card;
 import com.blitzar.cards.repository.CardRepository;
-import com.blitzar.cards.service.validator.AddCardValidator;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
