912b9115bbe9a423c76a5281f585828a2497324a
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-03-25 23:32:18 +0100

    Adding properties to Card Table

--- src/main/resources/db/migration/V0001__db_init_schema.sql
+++ src/main/resources/db/migration/V0001__db_init_schema.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS card
 (
-    card_id                  BIGINT NOT NULL auto_increment PRIMARY KEY,
-    cardholder_name          VARCHAR(21) NOT NULL,
-    card_status              VARCHAR(15) NOT NULL,
-    daily_withdrawal_limit   int NOT NULL
---     card_number              VARCHAR(40) NOT NULL,
---     expiration_date          date NOT NULL,
+    card_id                 BIGINT NOT NULL auto_increment PRIMARY KEY,
+    cardholder_name         VARCHAR(21) NOT NULL,
+    card_number             VARCHAR(40) NOT NULL,
+    card_status             VARCHAR(15) NOT NULL,
+    daily_withdrawal_limit  int NOT NULL,
+    daily_payment_limit     int NOT NULL,
+    expiration_date         date NOT NULL
 --     security_code            VARCHAR(3) NOT NULL
 )
 engine=innodb;
